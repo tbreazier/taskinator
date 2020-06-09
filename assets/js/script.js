9ee9404f6d41798771b929786cbce80a78000722
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-08 21:41:42 -0500

    form-submit complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,17 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+
+    formEl.reset(); 
 
     createTaskEl(taskDataObj);
 };
