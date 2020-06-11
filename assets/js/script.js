7e7e1a12fb32242b7f52b17556245566fe5f71aa
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-11 12:29:32 -0500

    final drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,6 +179,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -197,8 +198,17 @@ var dropTaskHandler = function(event) {
       else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -211,3 +221,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
